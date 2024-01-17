721ee6ed52a66843ffe0fbc1d163e8b41ccd5c93
Oscar Chan <95953478+oschan77@users.noreply.github.com>
2024-01-17 14:14:11 +0800

    Add files via upload

---
+++ run_container.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+mkdir docker_ws
+cd docker_ws
+mkdir BAG
+mkdir PCD
+cd ..
+
+# Allow X server to be accessed from the local machine
+xhost +local:
+
+# Container name
+CONTAINER_NAME="unimap_fastlio2"
+
+# Run the Docker container
+docker run -itd \
+  --name=$CONTAINER_NAME \
+  --user mars_ugv \
+  --network host \
+  --ipc=host \
+  -v /home/$USER/fastlio2_docker/docker_ws:/home/mars_ugv/docker_ws \
+  --privileged \
+  --env="QT_X11_NO_MITSHM=1" \
+  --volume="/etc/localtime:/etc/localtime:ro" \
+  -v /dev/bus/usb:/dev/bus/usb \
+  --device=/dev/dri \
+  --group-add video \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --env="DISPLAY=$DISPLAY" \
+  oschan77/lscm_unimap_fastlio2:latest \
+  /bin/bash
---
+++ test.sh
@@ -0,0 +1,1 @@
+docker exec fastlio2 /bin/bash -c 'pwd ; cd /home/mars_ugv/ ; pwd'
