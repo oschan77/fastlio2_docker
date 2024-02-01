8a7eed0d396372e1130c3c1ccfae1e86b5bb386f
Oscar Chan <95953478+oschan77@users.noreply.github.com>
2024-02-01 15:17:08 +0800

    Update rosbag2pcd.sh

--- rosbag2pcd.sh
+++ rosbag2pcd.sh
@@ -3,14 +3,11 @@
 BAG_NAME=$1
 PLAY_SPEED=$2
 
-# Define a log file
-LOGFILE="./logfile.log"
-
 docker exec fastlio2 /bin/bash -c "
   cd /home/mars_ugv/catkin_ws ;
   source devel/setup.bash ;
   roslaunch fast_lio mapping_velodyne.launch rviz:=false & echo \$! > /tmp/roslaunch.pid ;
-" >> $LOGFILE 2>&1
+"
 
 sleep 5
 
@@ -19,7 +16,7 @@ docker exec fastlio2 /bin/bash -c "
   source devel/setup.bash ;
   cd /home/mars_ugv/docker_ws/BAG ;
   rosbag play ./$BAG_NAME -r $PLAY_SPEED ;
-" >> $LOGFILE 2>&1
+"
 
 sleep 5
 
@@ -28,4 +25,4 @@ docker exec fastlio2 /bin/bash -c "
   while kill -0 \$(cat /tmp/roslaunch.pid) 2> /dev/null; do
     sleep 1
   done ;
-" >> $LOGFILE 2>&1
+"
