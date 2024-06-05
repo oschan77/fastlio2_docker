83b70f33f3f66682f2d063d3261d622febc4d4d8
oschan77 <oschan77@gmail.com>
2024-06-05 11:53:47 +0800

    Fix code

--- rosbag2pcd.sh
+++ rosbag2pcd.sh
@@ -26,9 +26,3 @@ docker exec fastlio2 /bin/bash -c "
     sleep 1
   done ;
 "
-
-curl --location 'http://192.168.10.82:8000/user/create_file?description=test&user_name=breeze_db9&map_name=breeze_db9_1' \
---header 'Connection: close' \
---header 'Keep-Alive: True' \
---header 'token: glpat-CMh8iws4reRKWyYyeGvT' \
---form 'file=@"./docker_ws/PCD/scans.pcd"'
